076064b189e0fc650396922a89745f47d01563d3
SvetLanaPesh <124920308+SvetLanaPesh@users.noreply.github.com>
2023-03-08 09:15:51 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,6 +1,32 @@
 // Copyright 2021 NNTU-CS
 
 int cbinsearch(int *arr, int size, int value) {
-  //  поместить сюда реализацию алгоритма
-  return 0; // если ничего не найдено
+    int l = 0, r = size - 1;
+    int count = -1, count1 = 0;
+    while (l <= r) {
+        int m = l + (r - l) / 2;
+        if (arr[m] == value) {
+            count = m;
+            r = m - 1;
+        }
+        else if (arr[m] > value)
+            r = m - 1;
+        else
+            l = m + 1;
+    }
+    if (count == -1)
+        return 0;
+    int l1 = 0, r1 = size - 1;
+    while (l1 <= r1) {
+        int m = l1 + (r1 - l1) / 2;
+        if (arr[m] == value) {
+            count1 = m +1;
+            l1 = m + 1;
+        }
+        else if (arr[m] > value)
+            r1 = m - 1;
+        else
+            l1 = m + 1;
+    }
+    return count1 - count;
 }
